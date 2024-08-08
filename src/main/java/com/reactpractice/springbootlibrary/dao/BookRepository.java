81853b4749f010ad9db26f61e10090ab14d0f779
venkataswamybudige <82580597+venkataswamybudige@users.noreply.github.com>
2024-08-08 23:53:46 +0530

    code changes

--- src/main/java/com/reactpractice/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/reactpractice/springbootlibrary/dao/BookRepository.java
@@ -1,9 +1,10 @@
 package com.reactpractice.springbootlibrary.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.reactpractice.springbootlibrary.entity.Book;
-
+@CrossOrigin(origins = "http://localhost:3000")  // Replace with your client’s origin
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
