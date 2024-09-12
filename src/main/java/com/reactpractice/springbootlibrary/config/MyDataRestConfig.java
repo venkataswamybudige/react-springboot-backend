c4dfcd7048f88f77f84a15ca82fae754c7f4a617
bvenkataswamy <venkataswamyb07@gmail.com>
2024-09-12 18:53:38 -0500

    changed in config

--- src/main/java/com/reactpractice/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/reactpractice/springbootlibrary/config/MyDataRestConfig.java
@@ -1,32 +1,47 @@
 package com.reactpractice.springbootlibrary.config;
 
+import com.reactpractice.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import com.reactpractice.springbootlibrary.entity.Book;
-
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-	private String theAllowedOrigns = "http://localhost:3000";
+	private String theAllowedOrigins = "http://localhost:3000";
 
 	@Override
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-		HttpMethod[] theUnsupportedActions = { HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT };
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+													 CorsRegistry cors) {
+		HttpMethod[] theUnsupportedActions = {
+				HttpMethod.POST,
+				HttpMethod.PATCH,
+				HttpMethod.DELETE,
+				HttpMethod.PUT};
+
 		config.exposeIdsFor(Book.class);
+	//	config.exposeIdsFor(Review.class);
+	//	config.exposeIdsFor(Message.class);
 
-		disabledHttpMethods(Book.class, config, theUnsupportedActions);
-		/* Configure cors mapping */
-		cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigns);
+		disableHttpMethods(Book.class, config, theUnsupportedActions);
+	//	disableHttpMethods(Review.class, config, theUnsupportedActions);
+	//	disableHttpMethods(Message.class, config, theUnsupportedActions);
 
+		/* Configure CORS Mapping */
+		cors.addMapping(config.getBasePath() + "/**")
+				.allowedOrigins(theAllowedOrigins);
 	}
 
-	private void disabledHttpMethods(Class theClass, RepositoryRestConfiguration config,
-			HttpMethod[] theUnsupportedActions) {
-		config.getExposureConfiguration().forDomainType(theClass)
-				.withItemExposure((metaData, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-				.withCollectionExposure((metaData, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+	private void disableHttpMethods(Class theClass,
+									RepositoryRestConfiguration config,
+									HttpMethod[] theUnsupportedActions) {
+		config.getExposureConfiguration()
+				.forDomainType(theClass)
+				.withItemExposure((metdata, httpMethods) ->
+						httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metdata, httpMethods) ->
+						httpMethods.disable(theUnsupportedActions));
 	}
-
 }
